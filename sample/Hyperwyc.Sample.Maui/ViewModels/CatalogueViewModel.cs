f513ba2b565a963649062494ff2daffafe695263
Matt Goldman <matt.goldman@outlook.com>
2026-08-17 21:01:38 +1000

    Added MVVM community toolkit, started adding ViewModel functionality

--- sample/Hyperwyc.Sample.Maui/ViewModels/CatalogueViewModel.cs
+++ sample/Hyperwyc.Sample.Maui/ViewModels/CatalogueViewModel.cs
@@ -1,6 +1,21 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Hyperwyc.Sample.Maui.Services;
+using Shared;
+
 namespace Hyperwyc.Sample.Maui.ViewModels;
 
-public class CatalogueViewModel
+public partial class CatalogueViewModel(ProductsApiClient productsClient) : ObservableObject
 {
-    
+    public ObservableCollection<Product> Products { get; set; } = [];
+
+    public async Task LoadProducts()
+    {
+        var apiProducts = await productsClient.GetProductsAsync();
+        Products = new ObservableCollection<Product>(apiProducts);
+    }
+
+    [RelayCommand]
+    private static Task SellProduct(Product product) => Shell.Current.GoToAsync("//sales/new", new ShellNavigationQueryParameters { { "product", product } });
 }
--- sample/Hyperwyc.Sample.Maui/ViewModels/LoginViewModel.cs
+++ sample/Hyperwyc.Sample.Maui/ViewModels/LoginViewModel.cs
@@ -1,6 +1,28 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Hyperwyc.Sample.Maui.Services;
+
 namespace Hyperwyc.Sample.Maui.ViewModels;
 
-public class LoginViewModel
+public partial class LoginViewModel(AuthenticationService authService) : ObservableObject
 {
-    
+    [ObservableProperty]
+    public partial string Email { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string Password { get; set; } = string.Empty;
+
+    [RelayCommand]
+    public async Task Login()
+    {
+        try
+        {
+            await authService.LoginAsync(Email, Password);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            await Application.Current!.Windows[0].Page!.DisplayAlertAsync("Error", e.Message, "OK");
+        }
+    }
 }
