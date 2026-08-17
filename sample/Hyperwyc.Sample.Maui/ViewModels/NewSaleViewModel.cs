209cba3818a5d9ba961dfb8ed78ceedb14c314c4
Matt Goldman <matt.goldman@outlook.com>
2026-08-18 08:15:08 +1000

    Started adding sale functionality to sample

--- sample/Hyperwyc.Sample.Maui/ViewModels/NewSaleViewModel.cs
+++ sample/Hyperwyc.Sample.Maui/ViewModels/NewSaleViewModel.cs
@@ -1,6 +1,47 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Hyperwyc.Sample.Maui.Services;
+using Shared;
+
 namespace Hyperwyc.Sample.Maui.ViewModels;
 
-public class NewSaleViewModel
+public partial class NewSaleViewModel(SalesApiClient client) : ObservableObject, IQueryAttributable
 {
-    
+    [ObservableProperty]
+    public partial bool ProductMissing { get; set; }
+
+    [ObservableProperty]
+    public partial int QuantityToSell { get; set; }
+
+    [ObservableProperty]
+    public partial Product? Product { get; set; }
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        Product = query["product"] as Product;
+        ProductMissing = Product == null;
+    }
+
+    private async Task RecordSale()
+    {
+        var sale = new Sale()
+        {
+            SoldAt      =  DateTime.UtcNow,
+            Quantity    =  QuantityToSell,
+            ProductId   =  Product!.Id,
+        };
+
+        var result = await client.RecordSaleAsync(sale);
+
+
+    }
+
+    private bool ValidateSale(Sale soldItem, Sale? returnedItem)
+    {
+        if (returnedItem is null) return false;
+
+        return soldItem.Id == returnedItem.Id
+            && soldItem.SoldAt == returnedItem.SoldAt
+            && soldItem.Quantity == returnedItem.Quantity
+            && soldItem.ProductId == returnedItem.ProductId;
+
+    }
 }
--- sample/Hyperwyc.Sample.Maui/ViewModels/SalesViewModel.cs
+++ sample/Hyperwyc.Sample.Maui/ViewModels/SalesViewModel.cs
@@ -1,6 +1,18 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Hyperwyc.Sample.Maui.Services;
+using Shared;
+
 namespace Hyperwyc.Sample.Maui.ViewModels;
 
-public class SalesViewModel
+public partial class SalesViewModel(SalesApiClient client) : ObservableObject
 {
-    
+    public ObservableCollection<Sale> Sales { get; set; } = [];
+
+    public async Task LoadApiSales()
+    {
+        var apiSales = await client.GetSalesAsync();
+
+        Sales = new ObservableCollection<Sale>(apiSales);
+    }
 }
